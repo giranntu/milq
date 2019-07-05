dd36224f747a541b358cdd470b52718b28bc31c7
Ryan Shen <giranntu@gmail.com>
2019-07-05 11:19:42 +0800

    make it an docker based OpenCV 4.1 install script
    make it an docker based OpenCV 4.1 install script

--- scripts/bash/install-opencv.sh
+++ scripts/bash/install-opencv.sh
@@ -21,7 +21,7 @@ OPENCV_VERSION='4.1.0'    # Version to be installed
 
 # 1. KEEP UBUNTU OR DEBIAN UP TO DATE
 
-sudo apt-get -y update
+apt-get -y update
 # sudo apt-get -y upgrade       # Uncomment to install new versions of packages currently installed
 # sudo apt-get -y dist-upgrade  # Uncomment to handle changing dependencies with new vers. of pack.
 # sudo apt-get -y autoremove    # Uncomment to remove packages that are now no longer needed
@@ -30,47 +30,61 @@ sudo apt-get -y update
 # 2. INSTALL THE DEPENDENCIES
 
 # Build tools:
-sudo apt-get install -y build-essential cmake
+apt-get install -y build-essential cmake
 
 # GUI (if you want GTK, change 'qt5-default' to 'libgtkglext1-dev' and remove '-DWITH_QT=ON'):
-sudo apt-get install -y qt5-default libvtk6-dev
+apt-get install -y qt5-default libvtk6-dev
 
 # Media I/O:
-sudo apt-get install -y zlib1g-dev libjpeg-dev libwebp-dev libpng-dev libtiff5-dev libjasper-dev \
+apt-get install -y zlib1g-dev libjpeg-dev libwebp-dev libpng-dev libtiff5-dev libjasper-dev \
                         libopenexr-dev libgdal-dev
 
 # Video I/O:
-sudo apt-get install -y libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev \
+apt-get install -y libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev \
                         libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev yasm \
                         libopencore-amrnb-dev libopencore-amrwb-dev libv4l-dev libxine2-dev
 
 # Parallelism and linear algebra libraries:
-sudo apt-get install -y libtbb-dev libeigen3-dev
+apt-get install -y libtbb-dev libeigen3-dev
 
 # Python:
-sudo apt-get install -y python-dev python-tk python-numpy python3-dev python3-tk python3-numpy
+apt-get install -y python-dev python-tk python-numpy python3-dev python3-tk python3-numpy
 
 # Java:
-sudo apt-get install -y ant default-jdk
+apt-get install -y ant default-jdk
 
 # Documentation:
-sudo apt-get install -y doxygen
+apt-get install -y doxygen
 
 
 # 3. INSTALL THE LIBRARY
 
-sudo apt-get install -y unzip wget
+apt-get install -y unzip wget
 wget https://github.com/opencv/opencv/archive/${OPENCV_VERSION}.zip
 unzip ${OPENCV_VERSION}.zip && rm ${OPENCV_VERSION}.zip
 mv opencv-${OPENCV_VERSION} OpenCV
 cd OpenCV && mkdir build && cd build
-cmake -DWITH_QT=ON -DWITH_OPENGL=ON -DFORCE_VTK=ON -DWITH_TBB=ON -DWITH_GDAL=ON \
-      -DWITH_XINE=ON -DBUILD_EXAMPLES=ON -DENABLE_PRECOMPILED_HEADERS=OFF ..
-make -j4
-sudo make install
-sudo ldconfig
 
+cmake -D WITH_CUDA=ON \
+    -D ENABLE_FAST_MATH=1 \
+    -D CUDA_FAST_MATH=1 \
+    -D WITH_CUBLAS=1 \
+    -DWITH_QT=ON \
+    -DWITH_OPENGL=ON \
+    -DFORCE_VTK=ON \
+    -DWITH_TBB=ON \
+    -DWITH_GDAL=ON \
+    -DWITH_XINE=ON \
+    -DBUILD_EXAMPLES=ON \
+    -D WITH_OPENCL=ON \
+    -D WITH_NVCUVID=ON \ 
+    -D WITH_GDAL=ON ..  
+    
+echo "Start OpenCV Build"
+make -j "$(nproc)"
+echo "Install OpenCV Build"
 
-# 4. EXECUTE SOME OPENCV EXAMPLES AND COMPILE A DEMONSTRATION
+make install
+bash -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
+ldconfig
 
-# To complete this step, please visit 'http://milq.github.io/install-opencv-ubuntu-debian'.
